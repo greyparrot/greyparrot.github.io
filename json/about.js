f1bda4b479bfe768550344057cae9df921f21a56
Oluwaseunfunmi Omotunde <holhujay@localhost.localdomain>
2018-05-03 16:05:55 -0500

    initial portfolio deploy

---
+++ json/about.js
@@ -0,0 +1,7 @@
+var data = {
+  "name": "Seunfunmi Omotunde",
+  "title": "Full Stack Developer",
+  "picture": "./try.jpeg",
+  "about": `I am really curious about. Real problem solving application cutting across several industries like 
+  Education, Health etc.  I love learning new technologies that bring efficiencies and increased productivity to my workflow.`
+}
---
+++ json/blogs.js
@@ -0,0 +1,33 @@
+var data = [
+  {
+    "cat": "web all",
+    "caption": "My College Beat",
+    "background": "./assets/img/mcb.png",
+    "link": "https://mycollegebeat.com/"
+  },
+  {
+    "cat": "mobile all",
+    "caption": "Olango Vista",
+    "background": "assets/img/olango.png",
+    "link": "assets/img/olango.png"
+  },
+  {
+    "cat": "web all",
+    "caption": "Jobdecoder",
+    "background": "https://scontent.flos8-1.fna.fbcdn.net/v/t1.0-9/14925268_533278673535490_3829624483444598950_n.jpg?_nc_cat=0&oh=022da4e991239384dbba1b86a35fb948&oe=5B623A9F",
+    "link": "http://jobdecoder.com/"
+  },
+  {
+    "cat": "mobile all",
+    "caption": "Scratch, Talk to me with your scratches",
+    "background": "assets/img/scratch.jpg",
+    "link": "assets/img/scratch.jpg"
+  },
+  {
+    "cat": "mobile all",
+    "caption": "Scribble, Your Smart Note",
+    "background": "assets/img/scribble.jpg",
+    "link": "assets/img/scribble.jpg"
+  },
+
+]
